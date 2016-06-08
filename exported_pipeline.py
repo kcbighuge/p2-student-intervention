d4c27c62cc2078a7d3aad690347f40890f15b52d
kcbighuge <kcbighuge@gmail.com>
2016-06-08 09:35:01 -0500

    add TPOT pipeline

---
+++ exported_pipeline.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+# NOTE: Make sure that the class is labeled 'class' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR')
+training_indices, testing_indices = train_test_split(tpot_data.index, stratify = tpot_data['class'].values, train_size=0.75, test_size=0.25)
+
+
+result1 = tpot_data.copy()
+
+# Perform classification with a random forest classifier
+rfc1 = RandomForestClassifier(n_estimators=198, max_features=min(608, len(result1.columns) - 1))
+rfc1.fit(result1.loc[training_indices].drop('class', axis=1).values, result1.loc[training_indices, 'class'].values)
+result1['rfc1-classification'] = rfc1.predict(result1.drop('class', axis=1).values)
